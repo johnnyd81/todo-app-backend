79f9d8d0b82b3493725bc690739fed4f120ad83f
Jonathan <johnnydjohnson81@gmail.com>
2024-01-09 17:13:33 +0200

    modified file

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -8,11 +8,17 @@ const requireAuth = async (req, res, next) => {
     res.status(401).json({ error: "Authorization token is required" });
   }
 
+  console.log("The authorization token looks like this: ", authorization);
+
   const token = authorization.split(" ")[1];
 
   try {
     //destructured the _id from the payload of the token
     const { _id } = jwt.verify(token, process.env.SECRET);
+    console.log(
+      "The verify of jwt returns this: ",
+      jwt.verify(token, process.env.SECRET)
+    );
 
     req.user = await User.findOne({ _id }).select("_id");
     next(); //move to next middleware
